a3bc4f5bf7b5e6e2c2aa6551eb799f3f53e9d17d
madhumohan570 <165363208+madhumohan570@users.noreply.github.com>
2025-01-06 15:18:56 +0530

    Update CucumberRunner.java

--- src/test/java/TestRunner/CucumberRunner.java
+++ src/test/java/TestRunner/CucumberRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		tags="@RegisterNewAccount", 
+		tags="@LoginStep", 
         features= {"src/test/resources/Features"},
         glue= {"StepDefinitions"}, 
         plugin= {"pretty"})
